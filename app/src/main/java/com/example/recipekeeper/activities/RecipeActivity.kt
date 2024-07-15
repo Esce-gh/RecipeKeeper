8c000a137373c06415142892da94fa536655327d
Esce <p.demski100@gmail.com>
2024-07-15 21:09:11 +0200

    RecipeActivity new features and UI changes

--- app/src/main/java/com/example/recipekeeper/activities/RecipeActivity.kt
+++ app/src/main/java/com/example/recipekeeper/activities/RecipeActivity.kt
@@ -4,63 +4,123 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.example.recipekeeper.R
 import com.example.recipekeeper.adapters.ItemAdapter
+import com.example.recipekeeper.adapters.RecipePagerAdapter
 import com.example.recipekeeper.models.Recipe
 import com.example.recipekeeper.scraper.Ingredient
 import com.example.recipekeeper.utils.FileManager
 import com.example.recipekeeper.utils.Redirect
-import java.util.ArrayList
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
-class RecipeActivity : AppCompatActivity( ){
+class RecipeActivity : AppCompatActivity() {
+    private lateinit var url: String
+    private lateinit var name: String
+    private lateinit var items: ArrayList<Ingredient>
+
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe)
 
         val recipe = intent.getSerializableExtra("RECIPE", Recipe::class.java)
-        val name = recipe?.name ?: ""
-        val url = recipe?.url ?: ""
-        val items = recipe?.ingredients ?: ArrayList()
+        name = recipe?.name ?: ""
+        url = recipe?.url ?: ""
+        items = recipe?.ingredients ?: ArrayList()
 
-        val textViewName: TextView = findViewById(R.id.textViewName)
-        textViewName.text = name
-        val buttonLink: Button = findViewById(R.id.buttonLink)
-        if (url.trim() == "") {
-            buttonLink.visibility = View.INVISIBLE
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+            title = name
         }
-        buttonLink.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse(url)
-            startActivity(intent)
-        }
-
-        val recyclerView: RecyclerView = findViewById(R.id.recyclerViewIngredients)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = ItemAdapter(items)
-
-        val buttonRemove: Button = findViewById(R.id.buttonRemove)
-        buttonRemove.setOnClickListener {
-            FileManager.deleteRecipe(this, name)
-            Redirect.redirect(this, SearchActivity::class.java)
+        toolbar.setNavigationOnClickListener {
+            finish()
         }
 
-        val buttonEdit: Button = findViewById(R.id.buttonEdit)
-        buttonEdit.setOnClickListener {
-            val intent = Intent(this, EditActivity::class.java).apply {
-                putExtra("DATA", items)
-                putExtra("NAME", name)
-                putExtra("URL", url)
-                putExtra("EDIT", true)
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        val tabLayout: TabLayout = findViewById(R.id.tab_layout)
+        val pagerAdapter = RecipePagerAdapter(this, items)
+        viewPager.adapter = pagerAdapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> "Ingredients"
+                1 -> "Instructions"
+                2 -> "Notes"
+                else -> ""
             }
-            startActivity(intent)
+        }.attach()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.recipe_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_delete -> {
+                deleteConfirmation()
+                true
+            }
+
+            R.id.action_edit -> {
+                val intent = Intent(this, EditActivity::class.java).apply {
+                    putExtra("DATA", items)
+                    putExtra("NAME", name)
+                    putExtra("URL", url)
+                    putExtra("EDIT", true)
+                }
+                startActivity(intent)
+                true
+            }
+
+            R.id.action_open -> {
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(url)
+                startActivity(intent)
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        super.onPrepareOptionsMenu(menu)
+        if (url.trim() == "") {
+            val urlMenuItem = menu?.findItem(R.id.action_open)
+            urlMenuItem?.isVisible = false
+        }
+        return true
+    }
+
+    private fun deleteConfirmation() {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Are you sure you want to delete this recipe?")
+            .setPositiveButton("Delete") { dialog, id ->
+                FileManager.deleteRecipe(this, name)
+                Redirect.redirect(this, SearchActivity::class.java)
+            }
+            .setNegativeButton("Cancel") { dialog, id ->
+                dialog.dismiss()
+            }
+        builder.create().show()
     }
 }
---
+++ app/src/main/java/com/example/recipekeeper/adapters/RecipePagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.recipekeeper.adapters
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.recipekeeper.fragments.IngredientsFragment
+import com.example.recipekeeper.fragments.InstructionsFragment
+import com.example.recipekeeper.fragments.NotesFragment
+import com.example.recipekeeper.scraper.Ingredient
+
+class RecipePagerAdapter(fragmentActivity: FragmentActivity, val items: ArrayList<Ingredient>) : FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount(): Int = 3  // Number of fragments
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> IngredientsFragment(items)
+            1 -> InstructionsFragment()
+            2 -> NotesFragment()
+            else -> throw IllegalArgumentException("Invalid position: $position")
+        }
+    }
+}
